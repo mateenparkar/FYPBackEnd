76e4975f053a2095907fdb98ccdb3a2221c0ced0
mateenparkar <mateenparkar4@gmail.com>
2024-02-26 09:28:01 +0000

    UserBooks integration test

---
+++ src/test/java/org/fyp/integration/UserBooksIntegrationTest.java
@@ -0,0 +1,38 @@
+package org.fyp.integration;
+
+import io.dropwizard.configuration.ResourceConfigurationSourceProvider;
+import io.dropwizard.testing.junit5.DropwizardAppExtension;
+import io.dropwizard.testing.junit5.DropwizardExtensionsSupport;
+import org.checkerframework.checker.units.qual.A;
+import org.fyp.DropwizardWebServiceApplication;
+import org.fyp.DropwizardWebServiceConfiguration;
+import org.fyp.cli.UserBooksRequest;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
+@ExtendWith(DropwizardExtensionsSupport.class)
+public class UserBooksIntegrationTest {
+    static final DropwizardAppExtension<DropwizardWebServiceConfiguration> APP = new DropwizardAppExtension<>(
+            DropwizardWebServiceApplication.class, null,
+            new ResourceConfigurationSourceProvider()
+    );
+
+    @Test
+    public void getUserBooks_ShouldReturn200_WhenUserBooksExists(){
+        Response response = APP.client().target("http://localhost:8080/api/getUserBooks/1").request().get();
+
+        Assertions.assertEquals(200, response.getStatus());
+    }
+
+    @Test
+    public void addUserBooks_ShouldReturn200_WhenUserBooksAdded(){
+        UserBooksRequest userBooksRequest = new UserBooksRequest(1,1);
+        Response response = APP.client().target("http://localhost:8080/api/addBookToUser").request().post(Entity.entity(userBooksRequest, MediaType.APPLICATION_JSON_TYPE));
+        Assertions.assertEquals(200, response.getStatus());
+    }
+}
