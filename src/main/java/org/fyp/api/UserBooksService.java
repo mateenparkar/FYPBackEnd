bf10deac3058458c86c948cee48eccad692f0325
mateenparkar <mateenparkar4@gmail.com>
2024-04-15 11:59:44 +0100

    Changes made to throw 400 when book is liked

--- src/main/java/org/fyp/api/UserBooksService.java
+++ src/main/java/org/fyp/api/UserBooksService.java
@@ -2,6 +2,7 @@ package org.fyp.api;
 
 import org.fyp.cli.UserBooks;
 import org.fyp.cli.UserBooksRequest;
+import org.fyp.client.BookAlreadyLikedException;
 import org.fyp.client.FailedToAddUserBooksException;
 import org.fyp.client.FailedToGetUserBooksException;
 import org.fyp.db.UserBooksDao;
@@ -16,7 +17,24 @@ public class UserBooksService {
         this.userBooksDao = userBooksDao;
     }
 
-    public void addBookToUser(UserBooksRequest userBooksRequest) throws FailedToAddUserBooksException, SQLException {
+    public boolean isBookLikedByUser(int userId, int bookId) throws SQLException {
+        List<UserBooks> userBooks = userBooksDao.getUserBooks(userId);
+        for (UserBooks userBook : userBooks) {
+            if (userBook.getBook_id() == bookId) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void addBookToUser(UserBooksRequest userBooksRequest) throws FailedToAddUserBooksException, SQLException, BookAlreadyLikedException {
+        int userId = userBooksRequest.getUserId();
+        int bookId = userBooksRequest.getBookId();
+
+        if (isBookLikedByUser(userId, bookId)) {
+            throw new BookAlreadyLikedException();
+        }
+
         try{
             userBooksDao.addBookToUser(userBooksRequest.getUserId(), userBooksRequest.getBookId());
         }catch(SQLException e){
